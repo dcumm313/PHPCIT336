3e091d679f998fcb052dcb91f0133c5c5228d4a7
dcumm313 <dcumm313@gmail.com>
2018-02-28 23:35:43 -0500

    Add files via upload

---
+++ partone.php
@@ -0,0 +1,31 @@
+<?php
+$one = 1;
+$two = 2;
+$three = $one + $two;
+echo $three;
+
+echo "<br>";
+
+$txt = "fear";
+echo "I must not " . $txt . ".";
+
+echo "<br>";
+
+echo "Fear is the " . "mindkiller.";
+
+echo "<br>";
+
+echo "Fear " . "is the " . "little death.";
+
+echo "<br>";
+
+$number = date("H");
+
+if ($number % 2 == 0) {
+    echo "Even";
+} else {
+    echo "Odd";
+}
+
+
+?>
